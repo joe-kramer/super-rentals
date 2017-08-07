38b098d3f151b204420d67b117266823d5e92f90
Joe Kramer <joekramer1125@gmail.com>
2017-08-07 11:52:43 -0700

    add update method to rentals

--- app/routes/index.js
+++ app/routes/index.js
@@ -14,6 +14,15 @@ export default Ember.Route.extend({
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
       this.transitionTo('index');
+    },
+    update(rental, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!=="") {
+          rental.set(key,params[key]);
+        }
+      });
+      rental.save();
+      this.transitionTo('index');
     }
   }
 });
