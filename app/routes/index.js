514146a39989c113d4d84fe1211f546cc3ce7bee
Joe Kramer <joekramer1125@gmail.com>
2017-08-07 13:53:08 -0700

    add rental details component and adjust routing accordingly

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,22 +6,9 @@ export default Ember.Route.extend({
   },
 
   actions: {
-    destroyRental(rental) {
-      rental.destroyRecord();
-      this.transitionTo('index');
-    },
     saveRental3(params) {
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
-      this.transitionTo('index');
-    },
-    update(rental, params) {
-      Object.keys(params).forEach(function(key) {
-        if(params[key]!=="") {
-          rental.set(key,params[key]);
-        }
-      });
-      rental.save();
       this.transitionTo('index');
     }
   }
